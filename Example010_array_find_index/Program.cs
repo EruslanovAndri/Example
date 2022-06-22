035fc88cbc2523bbbb1a35677de1d9325e82aaef
EruslanovAndrei <eruslanov84as@gmail.com>
2022-06-22 23:45:31 +0300

    add Example010_array_find_index

---
+++ Example010_array_find_index/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {1,12,31,4,15,16,15,39,74};
+
+int n = array.Length;
+int find = 15;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
